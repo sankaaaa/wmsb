3965e1fdb01d5db0368300376ca9456a52b9046d
sankaaaa <olexa2404@gmail.com>
2025-10-17 23:37:37 +0200

    unit test added

---
+++ __tests__/models/itemModel.test.js
@@ -0,0 +1,40 @@
+const { TextEncoder, TextDecoder } = require("util");
+global.TextEncoder = TextEncoder;
+global.TextDecoder = TextDecoder;
+
+const mongoose = require("mongoose");
+const Item = require("../../backend/models/Item");
+
+describe("Item Model Schema", () => {
+    test("should have the correct schema fields", () => {
+        const schemaPaths = Object.keys(Item.schema.paths);
+
+        expect(schemaPaths).toEqual(
+            expect.arrayContaining([
+                "_id",
+                "name",
+                "quantity",
+                "price",
+                "description",
+                "type",
+                "photo",
+                "__v"
+            ])
+        );
+    });
+
+    test("should have correct data types for each field", () => {
+        const { schema } = Item;
+
+        expect(schema.path("name").instance).toBe("String");
+        expect(schema.path("quantity").instance).toBe("Number");
+        expect(schema.path("price").instance).toBe("Number");
+        expect(schema.path("description").instance).toBe("String");
+        expect(schema.path("type").instance).toBe("String");
+        expect(schema.path("photo").instance).toBe("String");
+    });
+
+    test("should use 'storage' as collection name", () => {
+        expect(Item.collection.name).toBe("storage");
+    });
+});
